806862466a67c70b3cee8178bbfa08d7b0d5596f
Dusan Simonovic <dusan037@gmail.com>
2018-01-28 09:52:52 +0100

    blog final

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,6 +1,8 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+
 import { fetchPosts } from '../actions';
 
 class PostsIndex extends Component {
@@ -9,10 +11,11 @@ class PostsIndex extends Component {
   }
 
   renderPosts() {
-    console.log(this.props.posts);
     return _.map(this.props.posts, post => {
       return (
-          <li key={post.id} className="list-group-item">{post.title}</li>
+          <li key={post.id} className="list-group-item">
+            <Link to={`/posts/${post.id}`}>{post.title}</Link>
+          </li>
       );
     });
   }
@@ -20,6 +23,11 @@ class PostsIndex extends Component {
   render() {
     return (
         <div>
+          <div className="text-xs-right">
+            <Link className="btn btn-primary" to="/posts/new">
+              Add a Post
+            </Link>
+          </div>
           <h3>Posts</h3>
           <ul className="list-group">
             {this.renderPosts()}
