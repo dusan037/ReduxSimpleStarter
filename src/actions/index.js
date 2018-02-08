24523f2b135feccc279b5ff279118ddc253baabc
Dusan Simonovic <dusan037@gmail.com>
2018-02-08 23:47:18 +0100

    auth-client middle

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,11 @@
 import axios from 'axios';
 import { browserHistory } from 'react-router';
-import {AUTH_ERROR, AUTH_USER} from "./types";
+import {AUTH_ERROR, UNAUTH_USER, AUTH_USER} from "./types";
 
 const ROOT_URL = 'http://localhost:3090';
 
 export function signinUser({ email, password }) {
   return function(dispatch) {
-    console.log("jea");
     // Submit email/password to the server
     axios.post(`${ROOT_URL}/signin`, { email, password })
       .then(response => {
@@ -26,9 +25,36 @@ export function signinUser({ email, password }) {
   }
 }
 
+export function signupUser({ email, password }) {
+    return function(dispatch) {
+        // Submit email/password to the server
+        axios.post(`${ROOT_URL}/signup`, { email, password })
+            .then(response => {
+                // If request is good..
+                // - Update state to indicate user is authenticated
+                dispatch({ type: AUTH_USER });
+                // - Save the JWT token
+                localStorage.setItem('token', response.data.token);
+                // - redirect to the route '/feature'
+                browserHistory.push('/feature');
+            })
+            .catch((response) => {
+                // If request is bad...
+                // - Show an error to the user
+                dispatch(authError(response.data.error))
+            });
+    }
+}
+
 export function authError(error) {
   return {
     type: AUTH_ERROR,
     payload: error
   }
+}
+
+export function signoutUser() {
+    localStorage.removeItem('token');
+
+    return {type: UNAUTH_USER}
 }
--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -4,7 +4,7 @@ import * as actions from '../../actions';
 
 class Signin extends Component {
   handleFormSubmit({ email, password }) {
-    //console.log(email, password);
+    // Need to do something to log user in
     this.props.signinUser({ email, password });
   }
 
@@ -39,7 +39,7 @@ class Signin extends Component {
 }
 
 function mapStateToProps(state) {
-  return { errorMessage: state.auh.error };
+  return { errorMessage: state.auth.error };
 }
 
 export default reduxForm({
